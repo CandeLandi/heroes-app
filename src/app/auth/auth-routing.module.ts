fd8ef099fce80b950e006c9bf9e2038eb059014a
Candela Landi <landicandela01@gmail.com>
2024-03-26 12:53:25 -0600

    create component

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [];
 
 @NgModule({
-  imports: [],
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+
   exports: [],
-  declarations: [],
+
   providers: [],
 })
-export class NameModule { }
+export class AuthRoutingModule { }

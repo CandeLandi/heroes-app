eb61d122c3ff04509aea17cf3dc967f19deffef9
Candela Landi <landicandela01@gmail.com>
2024-04-01 20:55:31 -0300

    fixed hero-page

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Hero } from '../interfaces/hero.interface';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { environments } from '../../../environments/environments.prod';
 
 @Injectable({
@@ -9,12 +9,19 @@ import { environments } from '../../../environments/environments.prod';
 })
 export class HeroesService {
 
-  private baseUrl: string = environments.baseUrl
+  private baseUrl: string = environments.baseUrl;
 
   constructor(private http: HttpClient) { }
 
 
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes`)
+  }
+
+  getHeroById( id: string ): Observable<Hero|undefined> {
+    return this.http.get<Hero>(`${this.baseUrl}/heroes/${ id }`)
+    .pipe(
+      catchError( error => of(undefined))
+    )
   }
 }

7456cfa7afb62f6b2fde0e56e49137b5ac32f5f7
Candela Landi <landicandela01@gmail.com>
2024-04-05 23:11:30 -0300

    finish project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  constructor(private authService: AuthService) { }
+
   title = 'heroesApp';
+
+
 }
--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,3 +1,6 @@
+import { Router } from '@angular/router';
+import { User } from '../../../auth/interfaces/user.interface';
+import { AuthService } from './../../../auth/services/auth.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -12,6 +15,18 @@ export class LayoutPageComponent {
     { label: 'Listado', icon: 'label', url: './list' },
     { label: 'Añadir', icon: 'add', url: './new-hero' },
     { label: 'Buscar', icon: 'search', url: './search' },
-
   ]
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  get user(): User | undefined {
+    return this.authService.currentUser
+  }
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['/auth/login'])
+  }
 }

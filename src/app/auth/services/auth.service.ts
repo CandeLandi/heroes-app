e1665e9dd17f28e84cb98258e71bc5d57cfb8ce9
Candela Landi <landicandela01@gmail.com>
2024-06-06 10:37:52 -0300

    change .env

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -23,7 +23,7 @@ export class AuthService {
   login(email: string, password: string): Observable<User> {
 
     //http.post('login), { email, password}
-    return this.http.get<User>(`${this.baseUrl}/users`)
+    return this.http.get<User>(`${this.baseUrl}/users/1`)
       .pipe(
         tap(user => this.user = user),
         tap(user => localStorage.setItem('token', 'ajshdasd.asdjajksd.'))
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environments = {
-    baseUrl: 'https://angular-heroes-crud-app.netlify.app'
+    baseUrl: 'https://6661baf8ade0d90b8b3a8621--angular-heroes-crud-app.netlify.app'
 }
